d34d1ce2019dc068306d6b57d44a7953bcb4c1b0
ClockeNessMnstr <locke.dftc@gmail.com>
2024-09-18 20:42:46 -0400

    Merge branch 'LawnMower_updates'

--- custom_components/mammotion/lawn_mower.py
+++ custom_components/mammotion/lawn_mower.py
@@ -60,17 +60,16 @@ class MammotionLawnMowerEntity(MammotionBaseEntity, LawnMowerEntity):
     def activity(self) -> LawnMowerActivity | None:
         """Return the state of the mower."""
 
-        if self.rpt_dev_status is None:
-            return None
-
-        mode = self.rpt_dev_status.sys_status
         charge_state = self.rpt_dev_status.charge_state
+        mode = self.rpt_dev_status.sys_status
+        if mode is None:
+            return None
 
         LOGGER.debug("activity mode %s", mode)
         if (
             mode == WorkMode.MODE_PAUSE
-            or mode == WorkMode.MODE_READY
-            and charge_state == 0
+            or (mode == WorkMode.MODE_READY
+            and charge_state == 0)
         ):
             return LawnMowerActivity.PAUSED
         if mode in (WorkMode.MODE_WORKING, WorkMode.MODE_RETURNING):
@@ -85,71 +84,110 @@ class MammotionLawnMowerEntity(MammotionBaseEntity, LawnMowerEntity):
         """Start mowing."""
         # check if job in progress
         #
-        if self.rpt_dev_status is None:
+        mode = self.rpt_dev_status.sys_status
+        if mode is None:
             raise HomeAssistantError(
                 translation_domain=DOMAIN, translation_key="device_not_ready"
             )
-        work_area = self.report_data.work.area >> 16
-
-        if work_area > 0 and (
-            self.rpt_dev_status.sys_status == WorkMode.MODE_PAUSE
-            or self.rpt_dev_status.sys_status == WorkMode.MODE_READY
+        
+        if (
+            mode == WorkMode.MODE_WORKING
+            or mode == WorkMode.MODE_PAUSE
+            or mode == WorkMode.MODE_READY
+            or mode == WorkMode.MODE_RETURNING
         ):
             try:
-                await self.coordinator.async_send_command("resume_execute_task")
-                return await self.coordinator.async_request_iot_sync()
+                if mode == WorkMode.MODE_RETURNING:
+                    trans_key = "dock_failed"
+                    await self.coordinator.async_send_command("cancel_return_to_dock")
+                    await self.coordinator.async_request_iot_sync()
+                    mode = self.rpt_dev_status.sys_status
+                if mode == WorkMode.MODE_PAUSE :
+                    trans_key = "resume_failed"
+                    await self.coordinator.async_send_command("resume_execute_task")
+                    return await self.coordinator.async_request_iot_sync()
+                if mode == WorkMode.MODE_READY:
+                    trans_key = "start_failed"
+                    await self.coordinator.async_plan_route()
+                    await self.coordinator.async_send_command("start_job")
+                    await self.coordinator.async_request_iot_sync()
             except COMMAND_EXCEPTIONS as exc:
                 raise HomeAssistantError(
-                    translation_domain=DOMAIN, translation_key="resume_failed"
+                    translation_domain=DOMAIN, translation_key=trans_key
                 ) from exc
-        try:
-            await self.coordinator.async_plan_route()
-            await self.coordinator.async_send_command("start_job")
-            await self.coordinator.async_request_iot_sync()
-        except COMMAND_EXCEPTIONS as exc:
-            raise HomeAssistantError(
-                translation_domain=DOMAIN, translation_key="start_failed"
-            ) from exc
-        finally:
-            self.coordinator.async_set_updated_data(
-                self.coordinator.manager.mower(self.coordinator.device_name)
-            )
+            finally:
+                self.coordinator.async_set_updated_data(
+                    self.coordinator.manager.mower(self.coordinator.device_name)
+                )
+        return
 
     async def async_dock(self) -> None:
         """Start docking."""
 
+        charge_state = self.rpt_dev_status.charge_state
         mode = self.rpt_dev_status.sys_status
         if mode is None:
-            mode = WorkMode.MODE_READY
-
-        try:
-            if mode == WorkMode.MODE_RETURNING:
-                await self.coordinator.async_send_command("cancel_return_to_dock")
-                return await self.coordinator.async_request_iot_sync()
-            if mode == WorkMode.MODE_WORKING:
-                await self.coordinator.async_send_command("pause_execute_task")
-            await self.coordinator.async_send_command("return_to_dock")
-            await self.coordinator.async_request_iot_sync()
-        except COMMAND_EXCEPTIONS as exc:
             raise HomeAssistantError(
-                translation_domain=DOMAIN, translation_key="dock_failed"
-            ) from exc
-        finally:
-            self.coordinator.async_set_updated_data(
-                self.coordinator.manager.mower(self.coordinator.device_name)
+                translation_domain=DOMAIN, translation_key="device_not_ready"
             )
+
+        if (
+            charge_state == 0 
+            and (
+                mode == WorkMode.MODE_WORKING
+                or mode == WorkMode.MODE_PAUSE
+                or mode == WorkMode.MODE_READY
+                or mode == WorkMode.MODE_RETURNING
+            )
+        ):
+            try:
+                if mode == WorkMode.MODE_RETURNING:
+                    trans_key = "dock_failed"
+                    await self.coordinator.async_send_command("cancel_return_to_dock")
+                    return await self.coordinator.async_request_iot_sync()
+                if mode == WorkMode.MODE_WORKING:
+                    trans_key = "pause_failed"
+                    await self.coordinator.async_send_command("pause_execute_task")
+                trans_key = "dock_failed"
+                await self.coordinator.async_send_command("return_to_dock")
+                await self.coordinator.async_request_iot_sync()
+            except COMMAND_EXCEPTIONS as exc:
+                raise HomeAssistantError(
+                    translation_domain=DOMAIN, translation_key=trans_key
+                ) from exc
+            finally:
+                self.coordinator.async_set_updated_data(
+                    self.coordinator.manager.mower(self.coordinator.device_name)
+                )
         return
 
     async def async_pause(self) -> None:
         """Pause mower."""
-        try:
-            await self.coordinator.async_send_command("pause_execute_task")
-            await self.coordinator.async_request_iot_sync()
-        except COMMAND_EXCEPTIONS as exc:
+        
+        mode = self.rpt_dev_status.sys_status
+        if mode is None:
             raise HomeAssistantError(
-                translation_domain=DOMAIN, translation_key="pause_failed"
-            ) from exc
-        finally:
-            self.coordinator.async_set_updated_data(
-                self.coordinator.manager.mower(self.coordinator.device_name)
+                translation_domain=DOMAIN, translation_key="device_not_ready"
             )
+            
+        if (
+            mode == WorkMode.MODE_WORKING
+            or mode == WorkMode.MODE_RETURNING
+        ):
+            try:
+                if mode == WorkMode.MODE_RETURNING:
+                    trans_key = "dock_failed"
+                    await self.coordinator.async_send_command("cancel_return_to_dock")
+                    return await self.coordinator.async_request_iot_sync()
+                trans_key = "pause_failed"
+                await self.coordinator.async_send_command("pause_execute_task")
+                await self.coordinator.async_request_iot_sync()
+            except COMMAND_EXCEPTIONS as exc:
+                raise HomeAssistantError(
+                    translation_domain=DOMAIN, translation_key=trans_key
+                ) from exc
+            finally:
+                self.coordinator.async_set_updated_data(
+                    self.coordinator.manager.mower(self.coordinator.device_name)
+                )
+        return
